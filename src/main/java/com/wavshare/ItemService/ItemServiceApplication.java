5e4991cc99ca08f8d8f0041ac58cf190edb914e3
Judah <i460958@FontysICT.nl>
2024-04-19 22:28:48 +0200

    Updated port and dependencies

--- src/main/java/com/wavshare/ItemService/ItemServiceApplication.java
+++ src/main/java/com/wavshare/ItemService/ItemServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@RestController
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
